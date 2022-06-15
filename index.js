65590375b7ac2968b38c5a2a0d9b13398095fd48
Cole Osmer <coaliethegoalie12@gmail.com>
2022-06-14 19:19:57 -0500

    for loops

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+//console.log(weeklyExpenseQuestions)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseInt(window.prompt(weeklyExpenseQuestions[i]));
+    weeklyExpenses = answer + weeklyExpenses
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseInt(window.prompt(monthlyExpenseQuestions[i]));
+    monthlyExpenses = answer + monthlyExpenses
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseInt(window.prompt(annualExpenseQuestions[i]));
+    annualExpenses = answer + annualExpenses
+}
